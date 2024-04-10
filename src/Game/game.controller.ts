fc038ae055ef3cd53799f89b6592bf42dd13ecd6
4rtemis-25 <121249378+4rtemis-25@users.noreply.github.com>
2024-04-10 17:00:57 -0500

    Complete CRUD

--- src/Game/game.controller.ts
+++ src/Game/game.controller.ts
@@ -1,18 +1,43 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from "@nestjs/common";
 import { GameService } from "./game.service";
 import { Game } from "@prisma/client";
 
 @Controller('games')
 export class GameController {
-    constructor(private readonly gameService: GameService) {}
+    constructor(private gameService: GameService) { }
 
     @Get()
-    async getAllGames(){
-        this.gameService.getAllGames()
+    async getAllGames() {
+        return this.gameService.getAllGames()
     }
 
     @Post()
-    async createGame(@Body() data: Game){
-        this.gameService.createGame(data)
+    async createGame(@Body() data: Game) {
+        return this.gameService.createGame(data)
+    }
+
+    @Get(':id')
+    async getGameById(@Param('id') id: string) {
+        const gameFound = await this.gameService.getGameById(Number(id))
+        if (!gameFound) throw new NotFoundException('Game does not exist')
+        return gameFound
+    }
+
+    @Delete(':id')
+    async deleteGame(@Param('id') id: string) {
+        try {
+            return await this.gameService.deleteGame(Number(id))
+        } catch (error) {
+            throw new NotFoundException("Game does not exist")
+        }
+    }
+
+    @Put(':id')
+    async updateGame(@Param('id') id: string, @Body() data: Game) {
+        try {
+            return await this.gameService.updateGame(Number(id), data)
+        } catch (error) {
+            throw new NotFoundException("Game does not exist")
+        }
     }
 }
